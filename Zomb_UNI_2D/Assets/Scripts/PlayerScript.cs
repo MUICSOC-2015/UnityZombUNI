55c6a24f07a9aa9efb49eaa40ea379e787eba7de
agentzh2m <agentzh2m@gmail.com>
2015-08-07 16:08:17 +0700

    Add Basic Player Script
    Add Walking and other stuff need to finish turning left, right etc.

---
+++ Zomb_UNI_2D/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	Rigidbody2D rigid;
+	public float spd;
+
+	// Use this for initialization
+	void Start () {
+
+		transform.position = new Vector2 (3f, 3.3f);
+		rigid = GetComponent<Rigidbody2D> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		this.rigid.velocity = new Vector3 (Input.GetAxis ("Horizontal") , Input.GetAxis ("Vertical") , 0) * spd;
+	
+	}
+}
