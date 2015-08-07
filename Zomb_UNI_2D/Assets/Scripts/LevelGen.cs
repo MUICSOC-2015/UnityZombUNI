7302de4d43f290b79c094b015e1430d9424e8243
agentzh2m <agentzh2m@gmail.com>
2015-08-07 15:27:16 +0700

    Added Level Generation Script
    The Board is 10 times 10 and the wall in total it is 11 by 11 in total

---
+++ Zomb_UNI_2D/Assets/Scripts/LevelGen.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGen : MonoBehaviour {
+
+
+	public GameObject ground;
+	public GameObject wall;
+
+	// Use this for initialization
+	void Start () {
+		// create ground
+		for (int y = 0; y < 10; y++) {
+			for (int x = 0; x < 10; x++) {
+				Instantiate(ground, new Vector2(x, y), Quaternion.identity);
+			}
+		}
+
+		for (int i = 0; i < 11; i++) {
+			// create wall
+			Instantiate(wall, new Vector2(-1,i), Quaternion.identity);
+			Instantiate(wall, new Vector2(10, i), Quaternion.identity);
+			Instantiate(wall, new Vector2(i, 10), Quaternion.identity);
+			Instantiate(wall, new Vector2(i, -1), Quaternion.identity);
+
+		}
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}
