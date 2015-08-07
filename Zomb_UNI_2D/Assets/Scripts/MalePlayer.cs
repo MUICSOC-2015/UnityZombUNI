3b579e98553f376757c8cc2b931e1bfd121c8d20
agentzh2m <agentzh2m@gmail.com>
2015-08-08 00:02:37 +0700

    Add new male player animation
    Add another sprite that look a lot better

---
+++ Zomb_UNI_2D/Assets/Scripts/MalePlayer.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MalePlayer : MonoBehaviour {
+	
+	Rigidbody2D rigid;
+	public float spd;
+	Animator anim;
+	
+	// Use this for initialization
+	void Start () {
+		
+		transform.position = new Vector2 (3f, 3.3f);
+		rigid = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		this.rigid.velocity = new Vector3 (Input.GetAxis ("Horizontal") , Input.GetAxis ("Vertical") , 0) * spd;
+		
+		if (Input.GetAxisRaw ("Horizontal") != 0 || Input.GetAxisRaw ("Vertical") != 0) {
+
+			anim.SetBool("IDLE", false);
+
+		} else {
+
+			//anim.enabled = false;
+			anim.SetBool("IDLE", true);
+			anim.SetBool ("WalkFront", false);
+			anim.SetBool ("WalkBack", false);
+			anim.SetBool ("WalkLeft", false);
+			anim.SetBool ("WalkRight", false);
+
+		}
+		
+		if (Input.GetAxisRaw ("Horizontal") > 0) {
+			anim.SetBool ("WalkFront", false);
+			anim.SetBool ("WalkBack", false);
+			anim.SetBool ("WalkLeft", false);
+			anim.SetBool ("WalkRight", true);
+		} else if (Input.GetAxisRaw ("Horizontal") < 0) {
+			
+			anim.SetBool ("WalkFront", false);
+			anim.SetBool ("WalkBack", false);
+			anim.SetBool ("WalkLeft", true);
+			anim.SetBool ("WalkRight", false);
+			
+		} else if (Input.GetAxisRaw ("Vertical") > 0) {
+			
+			anim.SetBool ("WalkFront", false);
+			anim.SetBool ("WalkBack", true);
+			anim.SetBool ("WalkLeft", false);
+			anim.SetBool ("WalkRight", false);
+			
+		} else if (Input.GetAxisRaw ("Vertical") < 0) {
+			
+			anim.SetBool ("WalkFront", true);
+			anim.SetBool ("WalkBack", false);
+			anim.SetBool ("WalkLeft", false);
+			anim.SetBool ("WalkRight", false);
+		} 
+
+		
+	}
+}
