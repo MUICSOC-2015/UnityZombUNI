5e3f9d0096f2fe69bd57716622bacb5defb53b16
agentzh2m <agentzh2m@gmail.com>
2015-08-07 17:14:46 +0700

    Add Cam controller and Animate Player Script
    now I need a map and some game mechanics!

---
+++ Zomb_UNI_2D/Assets/Scripts/CamControll.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamControll : MonoBehaviour {
+
+	public Transform target;
+	
+	// Use this for initialization
+	void Start () {
+
+		transform.position = new Vector3 (target.position.x, target.position.y, -1);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = Vector3.Lerp(transform.position ,new Vector3 (target.position.x, target.position.y, -1), Time.deltaTime * 10);
+	
+	}
+}
