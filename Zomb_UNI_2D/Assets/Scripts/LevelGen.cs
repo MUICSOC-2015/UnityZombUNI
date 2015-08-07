cdd5c806842766cefc81cf19770e5a8c8f890ac5
agentzh2m <agentzh2m@gmail.com>
2015-08-07 19:51:23 +0700

    Fix Level Generator and Add Sprite
    We swap Row and Column because it was wrong and add Male and Female
    sprite

--- Zomb_UNI_2D/Assets/Scripts/LevelGen.cs
+++ Zomb_UNI_2D/Assets/Scripts/LevelGen.cs
@@ -6,23 +6,30 @@ public class LevelGen : MonoBehaviour {
 
 	public GameObject ground;
 	public GameObject wall;
+	public int row;
+	public int col;
 
 	// Use this for initialization
 	void Start () {
 		// create ground
-		for (int y = 0; y < 10; y++) {
-			for (int x = 0; x < 10; x++) {
+		for (int y = 0; y < row; y++) {
+			for (int x = 0; x < col; x++) {
 				Instantiate(ground, new Vector2(x, y), Quaternion.identity);
 			}
 		}
 
-		for (int i = 0; i < 11; i++) {
+		for (int i = 0; i < col; i++) {
 			// create wall
-			Instantiate(wall, new Vector2(-1,i), Quaternion.identity);
-			Instantiate(wall, new Vector2(10, i), Quaternion.identity);
-			Instantiate(wall, new Vector2(i, 10), Quaternion.identity);
 			Instantiate(wall, new Vector2(i, -1), Quaternion.identity);
+			Instantiate(wall, new Vector2(i, row), Quaternion.identity);
 
+		}
+
+		for (int i = 0; i < row; i++) {
+			// create wall
+			Instantiate(wall, new Vector2(col, i), Quaternion.identity);
+			Instantiate(wall, new Vector2(-1,i), Quaternion.identity);
+			
 		}
 
 
