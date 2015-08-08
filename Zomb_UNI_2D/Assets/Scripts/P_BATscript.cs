a5d110ad67ead886a14ac663e6f55fdce6c2543e
Worapol <Hamuel@Piyawats-MacBook-Air.local>
2015-08-08 23:17:04 +0700

    Add Basic Battle system
    Unfinished buggy battle system need to fix attack animation

--- Zomb_UNI_2D/Assets/Scripts/P_BATscript.cs
+++ Zomb_UNI_2D/Assets/Scripts/P_BATscript.cs
@@ -7,8 +7,12 @@ public class P_BATscript : MonoBehaviour {
 	Animator anim;
 	Rigidbody2D rigid;
 	Vector2 current;
+	Vector2 currentTarget;
 	public Transform target;
 	public Button ATK;
+	public float time;
+	public float time2;
+	public bool back2;
 
 	// Use this for initialization
 	void Start () {
@@ -16,34 +20,87 @@ public class P_BATscript : MonoBehaviour {
 		anim = GetComponent<Animator> ();
 		rigid = GetComponent<Rigidbody2D> ();
 		current = transform.position;
+		currentTarget = target.position;
+		time2 = 1f;
 	
-
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
+	public void Update () {
+
+		bool slap = false;
+		bool back = false;
+
+		if (anim.GetBool ("Slap")) {
+			time -= Time.deltaTime;
+		}
+
+		if (time < 0) {
+
+			anim.SetBool ("Slap", false);
+			slap = true;
 
 
 		}
+
+		if (slap) {
+
+			anim.SetBool ("WalkR", true);
+			rigid.velocity = new Vector3 (current.x, current.y, 0) * Time.deltaTime * 20f;
+			back = true;
+			back2 = true;
+
+		}
+
+
+
+		if (transform.position.x >= current.x && back && back2) {
+			rigid.velocity = new Vector3 (0, 0, 0) * 0;
+			anim.SetBool ("WalkR", false);
+			anim.SetBool ("IDLE", true);
+			print ("notstop");
+		}
+
+	}
+
+
+		
+
+
+
+	
 
 	public void Attack (bool ATK ) {
 
 
 		if (ATK) {
 
-			anim.SetBool("WalkL",true);
-			rigid.velocity = new Vector3 (target.position.x, target.position.y,0) * Time.deltaTime * 50f;
-			//anim.SetBool("WalkL", false);
-//			anim.SetBool("Slap", true);
-//			anim.SetBool("Slap", false);
-//			anim.SetBool("WalkR", true);
-//			rigid.velocity = new Vector3 (current.x, current.y, 0) * Time.deltaTime * 10f;
-//			anim.SetBool("WalkR", false);
-			print ("Attack");
+
+			rigid.velocity = new Vector3 (target.position.x, target.position.y, 0) * Time.deltaTime * 20f;
+			anim.SetBool ("WalkL", true);
+			anim.SetBool ("IDLE", false);
+			time2 = 1f;
+			back2 = false;
 
 		}
 
-	
 	}
+	
+
+
+	
+
+	public void OnCollisionEnter2D (Collision2D col) {
+
+		anim.SetBool ("WalkL", false);
+		anim.SetBool ("Slap", true);
+		time = 1.2f;
+		Vector3 v = rigid.velocity;
+		v.y = 0.0f;
+		rigid.velocity = v;
+
+	}
+
+	
+
+
 }
