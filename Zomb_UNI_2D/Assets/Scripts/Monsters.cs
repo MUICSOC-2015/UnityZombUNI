4dd3f8e858efa5d32a1d234c1fcf1e5a60e51996
agentzh2m <agentzh2m@gmail.com>
2015-08-31 00:53:59 +0700

    Super Change
    Whole new Battle system with fully functional animation and skill. Need
    Item and Stats UI to be finished

---
+++ Zomb_UNI_2D/Assets/Scripts/Monsters.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+[System.Serializable]
+public class Monsters : MonoBehaviour {
+
+	public Stats monster;
+	public int BaseHP ;
+	public int CurHP ;
+	public int BaseMP ;
+	public int BaseATK ;
+	public int CurATK ;
+	public int BaseDEF ;
+	public int curDEF  ;
+	public float Evasion ;
+	
+	public void StatUpdate() {
+
+		BaseHP = monster.STR * 10;
+		BaseMP = monster.INT * 5;
+		BaseDEF = monster.STR * 2;
+		BaseATK = monster.STR * 2;
+		Evasion = monster.AGI * 0.1f;
+
+	}
+
+
+}
---
+++ Zomb_UNI_2D/Assets/Scripts/PStats.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+public class PStats : MonoBehaviour {
+
+	public Stats Player;
+	public int BaseHP ;
+	public int CurHP ;
+	public int BaseMP ;
+	public int BaseATK ;
+	public int CurATK ;
+	public int BaseDEF ;
+	public int curDEF  ;
+	public float Evasion ;
+	
+	public void StatUpdate() {
+
+		BaseHP = Player.STR * 10;
+		BaseMP = Player.INT * 5;
+		BaseDEF = Player.STR * 2;
+		BaseATK = Player.STR * 2;
+		Evasion = Player.AGI * 0.1f;
+
+
+	}
+
+}
